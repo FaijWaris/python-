a9296c848e9b3e8d960ac2e428697d295d9db482
FaijWaris <faijwaris692@gmail.com>
2025-06-07 15:04:47 +0530

    datatypes and operators

---
+++ 5_type.py
@@ -0,0 +1,13 @@
+a=32
+print(type(a)) # class int
+b=3.14
+print(type(b)) # class float
+c="hello"
+print(type(c)) # class str
+d=True
+print(type(d)) # class bool
+# To check the type of a variable, you can use the type() function.
+#typecasting
+a="31.2"
+b=float(a) # converting string to float
+print(type(b)) # class float
---
+++ 6_input.py
@@ -0,0 +1,4 @@
+a=input("Enter  number 1: ") # python me hamesha input hamesha string me letra hai isliye hmko usko  int me bdl ke use krna padta hai
+b=input("Enter another number: ")
+v=int(a) + int(b)  # converting input strings to integers
+print("The sum of the two numbers is:", v)  # prints the sum of the two numbers
