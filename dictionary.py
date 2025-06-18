3b3edc057946785d00b0ca03e745166149bf679f
FaijWaris <faijwaris692@gmail.com>
2025-06-18 13:36:14 +0530

    dict and stes

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+marks={
+    "harry":1,
+    "rohan":2,
+}
+# print(marks,type(marks))
+
+#dict methods
+print(marks.keys())  # returns keys
+print(marks.values())  # returns values 
+print(marks.items())  # returns key-value pairs
+# print(marks.get("harry"))  # returns value for key "harry"
+# print(marks.get("rohan"))  # returns value for key "rohan"
+
+
+print(marks.get("harry3"))#its gives none if key is not present
+print(marks["harry3"])#it will give error if key is not present
--- practice11.py
+++
@@ -1,4 +0,0 @@
-#replacing double space with single space
-name="faij is  a  good  boy  and"
-print(name.replace("  ",""))
-print(name)# string are immutable 
